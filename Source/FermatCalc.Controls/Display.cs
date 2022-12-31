5e32e5a13e2cef8a45d705e283ff7ec912d78a13
filmee24 <filmee24@gmail.com>
2022-12-31 16:33:55 +0100

    Add Background Property To Display

--- Source/FermatCalc.Controls/Display.cs
+++ Source/FermatCalc.Controls/Display.cs
@@ -6,9 +6,23 @@ namespace FermatCalc.Controls;
 
 public class Display : Control
 {
+    public static StyledProperty<IBrush> BackgroundProperty = AvaloniaProperty.Register<Display, IBrush>(nameof(Background), Brushes.Beige);
+
+    public IBrush Background
+    {
+        get
+        {
+            return GetValue(BackgroundProperty);
+        }
+        set
+        {
+            SetValue(BackgroundProperty, value);
+        }
+    }
+
     public override void Render(DrawingContext context)
     {
-        context.DrawRectangle(Brushes.Red, new Pen(Brushes.Black), new(0, 0, Width, Height), 5, 5);
+        context.DrawRectangle(Background, new Pen(Brushes.Black), new(0, 0, Width, Height), 5, 5);
     }
 
     protected override Size MeasureOverride(Size availableSize)
