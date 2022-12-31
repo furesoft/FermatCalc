da9fd57adf320dbcebdca8d43ff05580abbdd143
filmee24 <filmee24@gmail.com>
2022-12-31 16:11:14 +0100

    Add Basic Display Rendering

--- Source/FermatCalc.Controls/Display.cs
+++ Source/FermatCalc.Controls/Display.cs
@@ -1,8 +1,21 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
 
-namespace FermatCalc.Controls
+namespace FermatCalc.Controls;
+
+public class Display : Control
 {
-    public class Display : Control
+    public override void Render(DrawingContext context)
     {
+        context.DrawRectangle(Brushes.Red, new Pen(Brushes.Black), new(0, 0, Width, Height), 5, 5);
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        Width = availableSize.Width;
+        Height = availableSize.Height;
+
+        return base.MeasureOverride(availableSize);
     }
 }
