9d31eb95ae97d37848b0d1a4848fbc9e57f17049
filmee24 <filmee24@gmail.com>
2023-01-01 10:43:31 +0100

    Add Navigation

--- Source/FermatCalc/Views/MainView.axaml.cs
+++ Source/FermatCalc/Views/MainView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Material.Styles.Controls;
 
 namespace FermatCalc.Views;
 
@@ -7,5 +9,16 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        this.FindControl<Carousel>("PageCarousel").SelectionChanged += CalculatorPage_SelectionChanged;
+        base.OnApplyTemplate(e);
+    }
+
+    private void CalculatorPage_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        this.FindControl<NavigationDrawer>("drawer").LeftDrawerOpened = false;
     }
 }
