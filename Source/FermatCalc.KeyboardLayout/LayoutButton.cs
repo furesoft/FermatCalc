3531216a47d41630c2dfcc9b0a9a21610dbb5a17
filmee24 <filmee24@gmail.com>
2023-01-01 16:14:20 +0100

    Make LayoutButton Reactive

--- Source/FermatCalc.KeyboardLayout/LayoutButton.cs
+++ Source/FermatCalc.KeyboardLayout/LayoutButton.cs
@@ -1,8 +1,22 @@
-﻿namespace FermatCalc.KeyboardLayout;
+﻿using ReactiveUI;
 
-public class LayoutButton
+namespace FermatCalc.KeyboardLayout;
+
+public class LayoutButton : ReactiveObject
 {
+    private string _display;
+    private string _actionID;
     public int ID { get; set; }
-    public string Display { get; set; }
-    public string ActionID { get; set; }
+
+    public string Display
+    {
+        get { return _display; }
+        set { this.RaiseAndSetIfChanged(ref _display, value); }
+    }
+
+    public string ActionID
+    {
+        get { return _actionID; }
+        set { this.RaiseAndSetIfChanged(ref _actionID, value); }
+    }
 }
