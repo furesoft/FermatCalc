67653fc6490859bae82b5747456be1848e829c21
filmee24 <filmee24@gmail.com>
2023-01-01 10:43:48 +0100

    Add Pages

---
+++ Source/FermatCalc/Views/Pages/CalculatorPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FermatCalc.Views.Pages;
+
+public partial class CalculatorPage : UserControl
+{
+    public CalculatorPage()
+    {
+        InitializeComponent();
+    }
+}
