a30dbc5c81b140c8f0df4579be34293553b7bd60
filmee24 <filmee24@gmail.com>
2023-01-01 11:54:30 +0100

    Add Back Button Support For Android

--- Source/FermatCalc.Android/MainActivity.cs
+++ Source/FermatCalc.Android/MainActivity.cs
@@ -1,10 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Avalonia.Android;
+using Avalonia.Controls.ApplicationLifetimes;
+using FermatCalc.ViewModels;
 
 namespace FermatCalc.Android;
 
 [Activity(Label = "FermatCalc.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
 public class MainActivity : AvaloniaMainActivity
 {
+    public override void OnBackPressed()
+    {
+        if (Avalonia.Application.Current.ApplicationLifetime is ISingleViewApplicationLifetime lifetime)
+        {
+            if (lifetime.MainView.DataContext is MainViewModel vm)
+            {
+                vm.SelectedPage--;
+            }
+        }
+    }
 }
