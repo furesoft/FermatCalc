7d9f4e1f1030619c6abbd378d0fe315ca53d62a0
filmee24 <filmee24@gmail.com>
2023-01-07 17:26:08 +0100

    Refactor ActionItems

---
+++ Source/FermatCalc/ActionItems/ResultActionItem.cs
@@ -0,0 +1,20 @@
+﻿using AuroraModularis.Core;
+using FermatCalc.Controls;
+
+namespace FermatCalc.ActionItems;
+
+public class ResultActionItem : DefaultActionItem
+{
+    public ResultActionItem()
+    {
+        Display = "equals";
+    }
+
+    public override void Invoke()
+    {
+        CalculationEngine.AppendToBuffer("1+2");
+
+        CalculationEngine.Evaluate();
+        Container.Current.Resolve<Display>().Result = CalculationEngine.Result;
+    }
+}
